f3df5c00110412769459c01922e1edc5af4f42cc
Timothy Graupmann <tim.graupmann@razer.com>
2021-02-08 00:13:57 -0800

    Made a test for simulating connection problems

---
+++ server_unreliable_test.js
@@ -0,0 +1,35 @@
+// From the samples - https://github.com/websockets/ws#server-broadcast
+// A client WebSocket broadcasting to every other connected WebSocket clients, excluding itself.
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 5050, path: '/chat' });
+
+// This is a server that has connection problems to see how clients handle connection issues.
+function setupSpottyConnection() {
+  wss.on('connection', function connection(ws) {
+
+    let timerClose = Math.floor(Math.random() * 1000);
+    // randomly close the connect in 0 to N seconds          
+    setTimeout(function () {
+      ws.close();
+    }, timerClose);
+
+    ws.on('message', function incoming(data) {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    });
+  });
+
+  wss.on('connection', function (ws) {
+    ws.on('error', console.error);
+  });
+
+  wss.on('close', function close() {
+    console.log('connection closed');
+  });
+}
+
+setupSpottyConnection();
